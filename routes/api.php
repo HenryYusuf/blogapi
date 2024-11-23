bdb972bf640b53c49f0c74238a389c0aac3d3496
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-11-23 15:39:45 +0700

    feat: auth sanctum for new app

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Laravel above 6 using auth:sanctum for simple auth
+
 Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/{id}', [PostController::class, 'show']);
 Route::post('posts', [PostController::class, 'store']);
